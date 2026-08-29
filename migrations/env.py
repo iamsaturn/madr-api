5d4b2996785f9259c78cbc30aab0ce0fc224cd7d
iamsaturn <arthusjonathan@gmail.com>
2026-08-29 03:17:54 +0200

    fix: alembic import formatting

--- migrations/env.py
+++ migrations/env.py
@@ -3,7 +3,7 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
-from madr_api import models # noqa: F401
+from madr_api import models  # noqa: F401
 from madr_api.database import table_registry
 from madr_api.settings import settings
 
